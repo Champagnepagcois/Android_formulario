e3cb9f5bc429e011abdc391e45e31d7372876316
Marlon Francois <marlonrorodriguez2b@gail.com>
2019-09-17 03:57:59 -0500

    ok
    1.1.0

---
+++ MyResourcesApp1 - copia/app/src/main/java/mx/ipn/myresourcesapp/MainActivity.java
@@ -0,0 +1,16 @@
+package mx.ipn.myresourcesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.myresourcesapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
